55e70898880062be859e2f75cdc48a765cbb8caa
dita.mahardika <setiaariadi@gmail.com>
2023-10-12 18:59:38 +0700

    tambah data

---
+++ database/migrations/2023_10_12_095410_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('namasiswa');
+            $table->string('jeniskelamin');
+            $table->string('alamat');
+            $table->biginteger('notelpon');
+            $table->string('tingkat');
+          
+           
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
